dfb927ffa7f066ca785e0fee76eded803790ab26
Ashwin <ashwinm.045@gmail.com>
2024-10-06 01:30:41 +0530

    Updated startUpScript by adding dummy manager

--- scripts/startUpScript.js
+++ scripts/startUpScript.js
@@ -1,12 +1,15 @@
 const runQuery = require('../db/runQuery')
 const { BUILDING_DATABASE } = require('../lib/constants')
+const hashPassword = require('../lib/hashPassword')
 
 const addOrgQuery = `INSERT INTO ${BUILDING_DATABASE}.organisation (\`name\`, \`domain\`, \`config\`) VALUES ('Sreedhanya', 'sreedhanya', '{}')`
+const addManager = `INSERT INTO ${BUILDING_DATABASE}.manager SET ?`
 
 const main = async () => {
   try {
-    await runQuery(BUILDING_DATABASE, addOrgQuery)
-    console.log('Organisation added successfully')
+    const orgID = (await runQuery(BUILDING_DATABASE, addOrgQuery))?.insertId
+    await runQuery(BUILDING_DATABASE, addManager , {name: 'testUser', username: 'test', password: await hashPassword('123456'), org_id: orgID})
+    console.log('Organisation and manager added successfully')
   } catch (error) {
     console.error('Error adding organisation:', error)
   }
