838ffaf0ebd2ccc4c99f8fdf038bc672e4c45aa7
Ashwin <ashwinm.045@gmail.com>
2024-10-09 17:39:42 +0530

    Add log on validator

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,8 +1,11 @@
 const sendHTTPResponse = require('../lib/sendHTTPResponse')
+const Log = require('../log')
 const validateRequest = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body, { abortEarly: false })
-  if (error) 
+  if (error){
+    Log.error(`[Servv] | validateRequest | Error in validating request : ${error.message}`)
     return sendHTTPResponse.error(res, 'Validation error',  error.details.map(err => err.message), 400)
+  }
   next()
 }
 
