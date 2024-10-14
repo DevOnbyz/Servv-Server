3e022236cb63256466a87f2e0e67524d41194ddb
Ashwin <ashwinm.045@gmail.com>
2024-10-14 10:41:52 +0530

    Made username case sensitive

--- routes/auth/query.js
+++ routes/auth/query.js
@@ -1,6 +1,6 @@
 module.exports = {
   getAdminData(database) {
-    return `SELECT * FROM ${database}.admin where username = ?`
+    return `SELECT * FROM ${database}.admin where BINARY username = ?`
   },
   getAdminDataByID(database) {
     return `SELECT * FROM ${database}.admin where id = ?`
