f408b788b835e043ec1937a0a7c3062c6e2bc4b3
Ashwin <ashwinm.045@gmail.com>
2024-10-18 23:54:02 +0530

    Added dmigration

--- db/dmigration.sql
+++ db/dmigration.sql
@@ -1,13 +1,3 @@
-ALTER TABLE resident DROP COLUMN name;
--- Remive forign key constraint
-ALTER TABLE resident DROP FOREIGN KEY fk_resident_ibfk_1;
-ALTER TABLE resident DROP FOREIGN KEY fk_resident_ibfk_2;
-ALTER TABLE resident DROP COLUMN org_id;
-ALTER TABLE resident DROP COLUMN ph_num;
-ALTER TABLE resident DROP COLUMN project_id;
-ALTER TABLE resident ADD COLUMN firstname VARCHAR(255) NOT NULL AFTER id;
-ALTER TABLE resident_identity ADD COLUMN created_by INT AFTER id;
-ALTER TABLE resident ADD COLUMN lastname VARCHAR(255) NOT NULL AFTER firstname;
 ALTER TABLE apartment MODIFY COLUMN `name` VARCHAR(255) NULL;
 ALTER TABLE apartment ADD COLUMN `status` TINYINT DEFAULT 1 AFTER name;
 ALTER TABLE resident ADD COLUMN `status` TINYINT DEFAULT 1 AFTER email_id;
