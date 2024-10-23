ef58be5a5fb8e2377f160336b2ba47094e3bd1de
Ashwin <ashwinm.045@gmail.com>
2024-10-23 17:30:47 +0530

    Added new migration for admin

--- db/dmigration.sql
+++ db/dmigration.sql
@@ -4,3 +4,10 @@ ALTER TABLE resident ADD COLUMN `status` TINYINT DEFAULT 1 AFTER email_id;
 -- 22-12-2014
 ALTER TABLE resident ADD COLUMN `org_id` INT AFTER `status`;
 ALTER TABLE resident ADD CONSTRAINT fk_resident_ibfk_4 FOREIGN KEY (`org_id`) REFERENCES `organisation`(`id`);
+ALTER TABLE resident ADD COLUMN `org_id` INT AFTER `status`;
+
+ALTER TABLE admin drop column `name`;
+ALTER TABLE admin ADD COLUMN `firstname` VARCHAR(100) NOT NULL AFTER `id`;
+ALTER TABLE admin ADD COLUMN `lastname` VARCHAR(100) NOT NULL AFTER `firstname`;
+ALTER TABLE admin ADD COLUMN `email` VARCHAR(100)  AFTER `lastname`;
+ALTER TABLE admin ADD COLUMN `ph_num` VARCHAR(30)  AFTER `email`;
--- db/migration.sql
+++ db/migration.sql
@@ -42,7 +42,10 @@ CREATE TABLE `role_permission_rel` (
 
 CREATE TABLE `admin` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
+    `firstname` VARCHAR(100) NOT NULL,
+    `lastname` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100),
+    `ph_num` VARCHAR(30) NOT NULL,
     `username` VARCHAR(255) UNIQUE NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `org_id` INT NOT NULL,
