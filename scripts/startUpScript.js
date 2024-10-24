541107fcb206b4e7096fabcc8d9e915f2ff77d27
Ashwin <ashwinm.045@gmail.com>
2024-10-24 10:18:19 +0530

    Updated startuo script

--- scripts/startUpScript.js
+++ scripts/startUpScript.js
@@ -8,7 +8,7 @@ const addManager = `INSERT INTO ${BUILDING_DATABASE}.admin SET ?`
 const main = async () => {
   try {
     const orgID = (await runQuery(BUILDING_DATABASE, addOrgQuery))?.insertId
-    await runQuery(BUILDING_DATABASE, addManager , {firstname: 'test', lastname: 'user', username: 'test', password: await hashPassword('123456'), org_id: orgID})
+    await runQuery(BUILDING_DATABASE, addManager , {firstname: 'test', lastname: 'user', username: 'test', password: await hashPassword('123456'), org_id: orgID, ph_num: '+917999999999'})
     console.log('Organisation and admin added successfully')
   } catch (error) {
     console.error('Error adding organisation:', error)
