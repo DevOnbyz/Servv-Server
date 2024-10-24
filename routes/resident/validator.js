15e236b3dcb7dc40aafef416fae24ae1986b82f2
Ashwin <ashwinm.045@gmail.com>
2024-10-24 12:57:16 +0530

    Fixed emailID optional and allowed null

--- routes/resident/validator.js
+++ routes/resident/validator.js
@@ -3,15 +3,15 @@ const Joi = require('joi')
 const addResidentSchema = Joi.object({
   firstname: Joi.string().required(),
   lastname: Joi.string().required(),
-  phNum: Joi.string().optional(),
-  emailID: Joi.string().required(),
+  phNum: Joi.string().required(),
+  emailID: Joi.string().optional().allow(null),
   project: Joi.array().required(),
 })
 
 const editResidentSchema = Joi.object({
   firstname: Joi.string().optional(),
   lastname: Joi.string().optional(),
-  emailID: Joi.string().optional(),
+  emailID: Joi.string().optional().allow(null),
   project: Joi.array().optional(),
   status: Joi.number().optional(),
 })
