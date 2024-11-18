50f6e673334c49483f902ae9f5430d868d61168e
Ashwin <ashwinm.045@gmail.com>
2024-11-18 17:38:22 +0530

    Fix qurey issue

--- routes/issues/query.js
+++ routes/issues/query.js
@@ -47,7 +47,7 @@ module.exports = {
     WHEN I.status = ${ISSUE_STATUS.INPROGRESS} THEN '${ISSUE_STATUS_STRING.INPROGRESS}' 
     WHEN I.status = ${ISSUE_STATUS.CLOSED} THEN '${ISSUE_STATUS_STRING.CLOSED}' 
     WHEN I.status = ${ISSUE_STATUS.ONHOLD} THEN '${ISSUE_STATUS_STRING.ONHOLD}' END as status,
-     I.created_at, RI.ph_num as phNum, I.description, S.name as serviceType, I.preferred_time as time,
+     I.created_at, RI.ph_num as phNum, I.description, S.name as serviceType, I.preferred_time as time
     FROM ${database}.issue I
     left join ${database}.apartment A on I.apartment_id = A.id 
     left join ${database}.project P on A.project_id = P.id
