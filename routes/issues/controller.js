191202e6618f9b4d93bdf02572dc284d5b5a1c59
Ashwin <ashwinm.045@gmail.com>
2024-11-27 21:42:41 +0530

    Added image key

--- routes/issues/controller.js
+++ routes/issues/controller.js
@@ -56,7 +56,7 @@ exports.getIssuesUnderResidentController = async (request, response) => {
     for (const issue of issues) {
       const activeAgentAssignment = await runQueryOne(CONSTANTS.BUILDING_DATABASE, queryBuilder.getActiveSiteVisitByIssueID(CONSTANTS.BUILDING_DATABASE),[issue.id])
       issue.agentOTP = activeAgentAssignment ? activeAgentAssignment.otp_code : null
-      issue.img_src = issue.img_src?.split(',')
+      issue.img_src = issue.img_src ? issue.img_src.split(',') : null
       const issuesEvents = await runQuery(CONSTANTS.BUILDING_DATABASE, queryBuilder.getIssuesEvent(CONSTANTS.BUILDING_DATABASE),[issue.id])
       issue.issuesEvents = issuesEvents
     }
