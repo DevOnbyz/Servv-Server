29417fceec9e42b8295976532cc001f633a25e6e
Ashwin <ashwinm.045@gmail.com>
2024-12-07 13:51:55 +0530

    Added issueLogData in edit invoice

--- routes/issues/controller.js
+++ routes/issues/controller.js
@@ -1011,8 +1011,8 @@ exports.editInvoiceController = async (request, response) => {
 
     const issueLogData = {
       issue_id : issueID,
-      event_type : isDraft ? CONSTANTS.ISSUE_SUB_STATUS_STRING.ESTIMATE_DRAFT : CONSTANTS.ISSUE_SUB_STATUS_STRING.ESTIMATE_SENT,
-      sub_status : isDraft ? CONSTANTS.ISSUE_SUB_STATUS_NUM.ESTIMATE_DRAFT : CONSTANTS.ISSUE_SUB_STATUS_NUM.ESTIMATE_SENT,
+      event_type : isDraft ? CONSTANTS.ISSUE_SUB_STATUS_STRING.INVOICE_DRAFTED : CONSTANTS.ISSUE_SUB_STATUS_STRING.INVOICE_SENT,
+      sub_status : isDraft ? CONSTANTS.ISSUE_SUB_STATUS_NUM.INVOICE_DRAFTED : CONSTANTS.ISSUE_SUB_STATUS_NUM.INVOICE_SENT,
       creator_id : request.userID,
       creator_type : CONSTANTS.SERVV_USER_TYPE_NUM.ADMIN
     }
