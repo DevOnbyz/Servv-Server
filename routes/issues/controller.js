6d60bcd83bcfb7893f48a4d9b230b4f1ecdf39ad
ali-miyan <alimiyan1112@gmail.com>
2024-12-09 06:02:08 +0000

    code formatted

--- routes/issues/controller.js
+++ routes/issues/controller.js
@@ -589,11 +589,8 @@ exports.addAndSendEstimateController = async (request, response) => {
       Log.info(`[${domain} | OrganisationID:${orgID}] | addAndSendEstimateController | ResidentFCMToken: ${residentFCMToken} | IssueID: ${issueID} | Notification sent successfully`)
       blastPushNotification(residentFCMToken, 'Estimate Generated' ,`An estimate has been generated for your service request.`)
     }
-
-    const successMessage = isDraft ? 'Estimate drafted successfully' : 'Estimate sent successfully'
-
     Log.info(`[${domain} | OrganisationID:${orgID}] | addAndSendEstimateController | Estimate ${isDraft ?"Drafted":"Sent"} successfully | IssueID: ${issueID}`)
-    return sendHTTPResponse.success(response, 'Estimate Sent successfully', {logID})
+    return sendHTTPResponse.success(response, `Estimate ${isDraft ?"Drafted":"Sent"} successfully`, {logID})
   } catch (error) {
     Log.error(`[${domain} | OrganisationID:${orgID}] | addAndSendEstimateController | ${error.message}`)
     sendHTTPResponse.error(response, 'Error while adding estimate', error.message)
