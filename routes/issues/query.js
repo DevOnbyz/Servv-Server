4cd69e75f6d5a0c2b6f1c950649b8341813f3619
Ashwin <ashes192000@gmail.com>
2024-12-26 20:36:09 +0000

    Add isWorkFeedbackCompleted status

--- routes/issues/query.js
+++ routes/issues/query.js
@@ -251,13 +251,13 @@ module.exports = {
     return `SELECT * FROM ${database}.agent_assignment where issue_id = ? AND status = ${AGENT_ASSIGNMENT_STATUS.PENDING} ORDER BY id DESC LIMIT 1`;
   },
   getActiveSiteVisitByIssueID(database) {
-    return `SELECT * FROM ${database}.agent_assignment where issue_id = ? AND status = ${AGENT_ASSIGNMENT_STATUS.PENDING} AND type = ${AGENT_ASSIGNMENT_TYPE.SITE_VISIT} LIMIT 1`;
+    return `SELECT * FROM ${database}.agent_assignment where issue_id = ? AND status = ${AGENT_ASSIGNMENT_STATUS.PENDING} AND type = ${AGENT_ASSIGNMENT_TYPE.SITE_VISIT} ORDER BY id DESC LIMIT 1`;
   },
   getActiveWorkOrderByIssueID(database) {
-    return `SELECT * FROM ${database}.agent_assignment where issue_id = ? AND status = ${AGENT_ASSIGNMENT_STATUS.PENDING} AND type = ${AGENT_ASSIGNMENT_TYPE.WORK_ORDER} LIMIT 1`;
+    return `SELECT * FROM ${database}.agent_assignment where issue_id = ? AND status = ${AGENT_ASSIGNMENT_STATUS.PENDING} AND type = ${AGENT_ASSIGNMENT_TYPE.WORK_ORDER} ORDER BY id DESC LIMIT 1`;
   },
   getCompletedWorkOrderByIssueID(database) {
-    return `SELECT * FROM ${database}.agent_assignment where issue_id = ? AND status = ${AGENT_ASSIGNMENT_STATUS.COMPLETED} AND type = ${AGENT_ASSIGNMENT_TYPE.WORK_ORDER} LIMIT 1`;
+    return `SELECT * FROM ${database}.agent_assignment where issue_id = ? AND status = ${AGENT_ASSIGNMENT_STATUS.COMPLETED} AND type = ${AGENT_ASSIGNMENT_TYPE.WORK_ORDER} ORDER BY id DESC LIMIT 1`;
   },
   getCompletedIssueByIssueID(database) {
     return `SELECT * FROM ${database}.issue WHERE id = ? AND status = ${ISSUE_STATUS.CLOSED} LIMIT 1`;
