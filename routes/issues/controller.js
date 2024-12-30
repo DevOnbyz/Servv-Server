3b1a9247448f427e01fdaf443eb3d9fa6c821bad
ali-miyan <alimiyan1112@gmail.com>
2024-12-30 14:00:08 +0000

    added isSkipped value to customer_preferred_time

--- routes/issues/controller.js
+++ routes/issues/controller.js
@@ -81,15 +81,15 @@ exports.getIssueByIDController = async (request, response) => {
   try {
     const issue = await runQueryOne(CONSTANTS.BUILDING_DATABASE, queryBuilder.getIssueByID(CONSTANTS.BUILDING_DATABASE), [issueID, orgID])
     const lastCompletetedWorkOrder = await runQueryOne(CONSTANTS.BUILDING_DATABASE, queryBuilder.getCompletedWorkOrderByIssueID(CONSTANTS.BUILDING_DATABASE), [issueID])
-    
+
     if (!issue) {
       Log.info(`[${domain} | OrganisationID:${orgID}  issueID:${issueID}] | getSingleIssueUnderResidentController | Issue not found`)
       return sendHTTPResponse.error(response, 'Issue not found', null, 404)
     }
-    
+
     const activeAgentAssignment = await runQueryOne(CONSTANTS.BUILDING_DATABASE, queryBuilder.getActiveAgentAssignment(CONSTANTS.BUILDING_DATABASE), [issueID])
     issue.agentOTP = activeAgentAssignment ? activeAgentAssignment.otp_code : null
-    issue.isWorkFeedbackCompleted = lastCompletetedWorkOrder?.is_satisfied == 1 || (lastCompletetedWorkOrder?.is_satisfied == 0 && ! _.isEmpty(lastCompletetedWorkOrder?.feedback_comments))
+    issue.isWorkFeedbackCompleted = lastCompletetedWorkOrder?.is_satisfied == 1 || (lastCompletetedWorkOrder?.is_satisfied == 0 && !_.isEmpty(lastCompletetedWorkOrder?.feedback_comments))
     issue.img_src = issue.img_src ? issue.img_src.split(',') : null
     issue.reviewed = issue.reviewed === CONSTANTS.REVIEW_STATUS.COMPLETED
     const issuesEventsQuery = isCustomer ? queryBuilder.getIssuesEventForCustomer(CONSTANTS.BUILDING_DATABASE) : queryBuilder.getIssuesEvent(CONSTANTS.BUILDING_DATABASE)
@@ -1159,9 +1159,9 @@ exports.addPreferredTimeController = async (request, response) => {
   const domain = request.domain
   const issueID = request.params.issueID
   try {
-    const { preferredDatetime } = request.body
+    const { preferredDatetime, isSkipped } = request.body
     const newIssueData = {
-      customer_preferred_time: moment.utc(preferredDatetime)?.format('YYYY-MM-DD HH:mm:ss')
+      customer_preferred_time: isSkipped ? null : moment.utc(preferredDatetime)?.format('YYYY-MM-DD HH:mm:ss')
     }
     await runQuery(CONSTANTS.BUILDING_DATABASE, queryBuilder.updateIssue(CONSTANTS.BUILDING_DATABASE), [newIssueData, issueID])
     Log.info(`[${domain} | OrganisationID:${orgID}] | addPreferredTimeController | The preferred time has been added successfully | IssueID: ${issueID}`)
@@ -1349,7 +1349,7 @@ exports.issueFeedbackController = async (request, response) => {
   try {
     const { starRating } = request.body
     const issueDetails = (await runQueryOne(CONSTANTS.BUILDING_DATABASE, queryBuilder.getIssuseByID(CONSTANTS.BUILDING_DATABASE), [issueID]))
-    
+
     // const completedIssue = (await runQueryOne(CONSTANTS.BUILDING_DATABASE, queryBuilder.getCompletedIssueByIssueID(CONSTANTS.BUILDING_DATABASE), [issueID]))
     // if (_.isEmpty(completedIssue)) return sendHTTPResponse.error(response, 'Issue is not completed yet!', null, 400)
 
