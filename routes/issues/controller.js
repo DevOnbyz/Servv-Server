7b63560d0c47029d9a5e2671b814a00240aa36ee
ali-miyan <alimiyan1112@gmail.com>
2025-01-14 11:01:44 +0000

    make is satisfied to constant

--- routes/issues/controller.js
+++ routes/issues/controller.js
@@ -62,7 +62,7 @@ exports.getIssuesUnderResidentController = async (request, response) => {
       issue.agentOTP = activeAgentAssignment ? activeAgentAssignment.otp_code : null
       issue.img_src = issue.img_src ? issue.img_src.split(',') : null
       const issueEventQuery = isCustomer ? queryBuilder.getIssuesEventForCustomer(CONSTANTS.BUILDING_DATABASE) : queryBuilder.getIssuesEvent(CONSTANTS.BUILDING_DATABASE)
-      issue.isWorkFeedbackCompleted = lastCompletetedWorkOrder?.is_satisfied == 1 || (lastCompletetedWorkOrder?.is_satisfied == 0 && !_.isEmpty(lastCompletetedWorkOrder?.feedback_comments))
+      issue.isWorkFeedbackCompleted = lastCompletetedWorkOrder?.is_satisfied == CONSTANTS.SATISFACTION_STATUS.SATISFIED || (lastCompletetedWorkOrder?.is_satisfied == CONSTANTS.SATISFACTION_STATUS.PENDING && !_.isEmpty(lastCompletetedWorkOrder?.feedback_comments))
       const issuesEvents = await runQuery(CONSTANTS.BUILDING_DATABASE, issueEventQuery, [issue.id])
       issue.reviewed = issue.reviewed === CONSTANTS.REVIEW_STATUS.COMPLETED
       issue.issuesEvents = issuesEvents
