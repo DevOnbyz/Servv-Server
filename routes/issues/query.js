079bc80d4922a1dd783a26937c26a2c6f9ff00fd
ali-miyan <alimiyan1112@gmail.com>
2025-01-17 10:58:55 +0000

    updated getIssuesEventForCustomer,make feedack only on work completd

--- routes/issues/query.js
+++ routes/issues/query.js
@@ -130,8 +130,8 @@ module.exports = {
         IE.creator_type, 
         (SELECT visit_scheduled_time FROM ${database}.agent_assignment WHERE issue_id = IE.issue_id AND IE.sub_status IN (${ISSUE_SUB_STATUS_NUM.SITE_VISIT_ASSIGNED}, ${ISSUE_SUB_STATUS_NUM.WORK_ASSIGNED}, ${ISSUE_SUB_STATUS_NUM.SITE_VISIT_COMPLETED}, ${ISSUE_SUB_STATUS_NUM.WORK_COMPLETED}) ORDER BY created_at DESC LIMIT 1) as event_time,
         IE.created_at,
-        AA.is_satisfied as isSatisfied,
-        AA.feedback_comments as feedbackComments,
+        (SELECT is_satisfied FROM ${database}.agent_assignment WHERE issue_id = IE.issue_id AND IE.sub_status = ${ISSUE_SUB_STATUS_NUM.WORK_COMPLETED} ORDER BY created_at DESC LIMIT 1) as isSatisfied,
+        (SELECT feedback_comments FROM ${database}.agent_assignment WHERE issue_id = IE.issue_id AND IE.sub_status = ${ISSUE_SUB_STATUS_NUM.WORK_COMPLETED} ORDER BY created_at DESC LIMIT 1) as feedbackComments,
         AA.updated_at
     FROM ${database}.issue_event IE
     LEFT JOIN ${database}.admin A ON IE.creator_id = A.id
