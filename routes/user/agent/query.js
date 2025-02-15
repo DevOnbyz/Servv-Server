37d911bb9079ed4775ba26fb861c5f921ba37a96
ali-miyan <alimiyan1112@gmail.com>
2025-02-15 05:30:22 +0000

    updated the agent assignment completed time

--- routes/user/agent/query.js
+++ routes/user/agent/query.js
@@ -90,7 +90,12 @@ module.exports = {
     WHEN AA.status = ${AGENT_ASSIGNMENT_STATUS.CANCELLED} THEN 'CANCELLED'
     END as status,
     CASE 
-    WHEN AA.status = ${AGENT_ASSIGNMENT_STATUS.COMPLETED} THEN IE.event_time 
+      WHEN AA.status = ${AGENT_ASSIGNMENT_STATUS.COMPLETED} THEN
+        CASE 
+          WHEN AA.type = ${AGENT_ASSIGNMENT_TYPE.SITE_VISIT} AND IE.sub_status = ${ISSUE_SUB_STATUS_NUM.SITE_VISIT_COMPLETED} THEN IE.event_time
+          WHEN AA.type = ${AGENT_ASSIGNMENT_TYPE.WORK_ORDER} AND IE.sub_status = ${ISSUE_SUB_STATUS_NUM.WORK_COMPLETED} THEN IE.event_time
+        ELSE NULL
+      END
     ELSE NULL 
     END as assignmentCompletedTime,
     AP.name as doorNo, P.name as projectName, S.name as serviceType, SOR.name as serviceSubTypeName,
@@ -106,7 +111,7 @@ module.exports = {
     LEFT JOIN ${database}.service_organisation_rel SOR ON SOR.id = I.service_subtype
     LEFT JOIN ${database}.resident R ON R.id = I.resident_id
     LEFT JOIN ${database}.resident_identity RI ON RI.id = R.identity_id
-    LEFT JOIN ${database}.issue_event IE ON IE.issue_id = I.id AND IE.sub_status IN (${ISSUE_SUB_STATUS_NUM.SITE_VISIT_COMPLETED}, ${ISSUE_SUB_STATUS_NUM.WORK_COMPLETED})
+    LEFT JOIN ${database}.issue_event IE ON IE.issue_id = I.id
     where AA.id = ? AND AA.agent_id = ? ORDER BY AA.created_at DESC`;
   }
 };
