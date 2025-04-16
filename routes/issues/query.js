3ecdfddddc4fe7c6b2c011a964cdeaba31515e74
shabeer <shabeerp15@gmail.com>
2025-04-16 15:51:38 +0530

    Add time slot in getWorkOrderUnderIssue query

--- routes/issues/query.js
+++ routes/issues/query.js
@@ -250,6 +250,7 @@ module.exports = {
   },
   getWorkOrderUnderIssue(database) {
     return `SELECT AA.id as id, AA.issue_id as issue_id, CONCAT(A.firstname, ' ', A.lastname) as assignee, AA.created_at as createdAt, AA.visit_scheduled_time as site_visit_time,
+    AA.time_slot as timeSlot,
     CASE 
     WHEN AA.visit_scheduled_time < CURDATE() THEN DATEDIFF(CURDATE(), AA.visit_scheduled_time)
     ELSE 0
